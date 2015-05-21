9b4b0b7e7371490fb2a4f5cf209f905e4664ef74
Bruce Park <brucepark0@gmail.com>
2015-05-21 11:12:30 -0700

    update environment.js variables for production

--- js/environment.js
+++ js/environment.js
@@ -1,15 +1,17 @@
 // UNCOMMENT OUT THE BLOCK THAT IS APPROPRIATE FOR YOUR ENVIRONMENT!!!!
 
 //For production
-/************
+
 var ENV_PROFILES_URL = 'http://profiles.sc-ctsi.org/';
 var ENV_LOCAL_URL = 'http://profiles.sc-ctsi.org/';
 var ENV_API_URL = 'http://scctsi-ctds-production.herokuapp.com/1/clinical_trials/';
-**************/
+var ENV_GADGET_URL = 'http://sc-ctsi.org/clinical_trials_gadget/ClinicalTrials.xml';
 
 //For staging
+/************
 var ENV_PROFILES_URL = 'http://profilesstage.sc-ctsi.org/';
 var ENV_LOCAL_URL = 'http://profilesstage.sc-ctsi.org/';
 
 var ENV_GADGET_URL = 'http://sc-ctsi.org/clinical_trials_gadget_staging/ClinicalTrials.xml';
 var ENV_API_URL = 'http://scctsi-ctds-staging.herokuapp.com/1/clinical_trials/';
+**************/
