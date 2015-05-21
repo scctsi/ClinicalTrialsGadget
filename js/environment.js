41691eab426562aa24f0d0e9b1c6d4e86bad588d
Bruce Park <brucepark0@gmail.com>
2015-05-21 11:08:38 -0700

    update profiles_url with usc staging url from ucsf staging url

--- js/environment.js
+++ js/environment.js
@@ -8,7 +8,7 @@ var ENV_API_URL = 'http://scctsi-ctds-production.herokuapp.com/1/clinical_trials
 **************/
 
 //For staging
-var ENV_PROFILES_URL = 'http://stage-profiles.ucsf.edu/profiles200';
+var ENV_PROFILES_URL = 'http://profilesstage.sc-ctsi.org/';
 var ENV_LOCAL_URL = 'http://profilesstage.sc-ctsi.org/';
 
 var ENV_GADGET_URL = 'http://sc-ctsi.org/clinical_trials_gadget_staging/ClinicalTrials.xml';
